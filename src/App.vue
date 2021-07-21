0d9266c2612f751b8d7cd996b215b9da38c73835
BrennnanPredmore <brennnann@gmail.com>
2021-07-20 22:57:33 -0500

    first vue commit on github EVER

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <div>
     <SearchBar></SearchBar>
-    <Hello></Hello>
   </div>
 </template>
 
 <script>
 import SearchBar from './Components/SearchBar.vue';
-import Hello from './Components/Hello.vue';
 export default {
   name: 'App',
   components: {
     SearchBar,
-    Hello,
+
   },
 };
 </script>
